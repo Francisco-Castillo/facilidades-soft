b2296727edcb36b0f2604e2adfe64b8c2c314e0b
Francisco Castillo <cesarfranciscocastillo@gmail.com>
2020-12-05 22:55:03 -0300

    cambio de nombres en tablas y campos, uso de notacion snake
    los nombres de tablas estan en plural y el de los campos en singular, si incluyen mas de una palabra
    se utiliza el caracter guion bajo para su separacion

--- src/main/resources/db/migration/V1__CrearTablas.sql
+++ src/main/resources/db/migration/V1__CrearTablas.sql
@@ -3,75 +3,75 @@
  * Created: 27/12/2019
  */
 
-CREATE TABLE Personas(
+CREATE TABLE personas(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   apellido VARCHAR(45) NOT NULL,
   nombre VARCHAR(45) NOT NULL,
-  ndocumento VARCHAR(15) NOT NULL,
-  fnacimiento DATE,
+  numero_documento VARCHAR(15) NOT NULL,
+  fecha_nacimiento DATE,
   sexo INT NOT NULL DEFAULT 0,
   email VARCHAR(50),
   telefono VARCHAR(20),
   imagen VARCHAR(50)
 );
 
-CREATE TABLE Clientes(
+CREATE TABLE clientes(
   id INT NOT NULL PRIMARY KEY,
   codigo VARCHAR(15),
-  falta DATETIME,
+  fecha_alta DATETIME,
   estado INT NOT NULL
 );
 
-CREATE TABLE Usuarios(
+CREATE TABLE usuarios(
   id INT NOT NULL PRIMARY KEY,
   username VARCHAR(15) NOT NULL,
   password VARCHAR(255) NOT NULL,
   estado INT NOT NULL,
-  falta DATE NOT NULL,
-  fultimologin DATETIME,
-  idtipousuario INT NOT NULL
+  fecha_alta DATE NOT NULL,
+  fecha_ultimo_login DATETIME,
+  id_tipo_usuario INT NOT NULL
 );
 
-CREATE TABLE Perfiles(
+CREATE TABLE perfiles(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   descripcion VARCHAR(20)
 );
 
-CREATE TABLE Prestamos(
+CREATE TABLE prestamos(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  dineroprestado DECIMAL(7,2) NOT NULL,
-  fsolicitud DATE NOT NULL,
-  cantidadcuotas INT NOT NULL,
+  dinero_prestado DECIMAL(7,2) NOT NULL,
+  fecha_solicitud DATE NOT NULL,
+  cantidad_cuotas INT NOT NULL,
   tna DECIMAL(7,2),
-  gastosadmin DECIMAL(3,2),
-  segvida DECIMAL(3,2),
+  gastos_admin DECIMAL(3,2),
+  seguro_vida DECIMAL(3,2),
   iva DECIMAL(3,2),
   observaciones VARCHAR(400), 
   estado INT NOT NULL,
-  idcliente INT NOT NULL,
-  idoperador INT NOT NULL
+  id_cliente INT NOT NULL,
+  id_operador INT NOT NULL
 );
 
-CREATE TABLE Cuotas(
+CREATE TABLE cuotas(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   nro INT NOT NULL,
   valor DECIMAL(7,2) NOT NULL,
-  fvencimiento DATE NOT NULL,
+  fecha_vencimiento DATE NOT NULL,
   estado INT NOT NULL,
-  idprestamo INT NOT NULL
+  id_prestamo INT NOT NULL
 );
 
-CREATE TABLE Pagos(
+CREATE TABLE pagos(
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  fechapago DATE NOT NULL,
-  idprestamo INT NOT NULL
+  fecha DATE NOT NULL,
+  id_prestamo INT NOT NULL
 );
 
-CREATE TABLE DetallePagos(
-  idpago INT NOT NULL,
-  idcuota INT NOT NULL,
+CREATE TABLE detalle_pagos(
+  id_pago INT NOT NULL,
+  id_cuota INT NOT NULL,
   recargo DECIMAL (7,2),
-  montoabonado DECIMAL(7,2)
+  monto_abonado DECIMAL(7,2)
 );
 
 
